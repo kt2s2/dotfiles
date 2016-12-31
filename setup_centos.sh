626922704af66b4887652d704c1a725de01207dd
sa-inu <uovo.aureo.000@gmail.com>
2016-12-31 14:17:58 +0900

    add install ruby

--- setup_centos.sh
+++ setup_centos.sh
@@ -1,7 +1,19 @@
 # sudo yum update -y
+
+# install node
 curl -L git.io/nodebrew | perl - setup
 echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.bash_profile
 source ~/.bash_profile
 exec $SHELL -l
 nodebrew install-binary stable
 nodebrew use stable
+
+# install ruby
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+exec $SHELL -l
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+rbenv install $(rbenv install -l | grep -v - | tail -1)
+rbenv rehash
+rbenv global $(rbenv install -l | grep -v - | tail -1)
