0672d3389bab493b7cd5313124ee765abf0a69d2
sa-inu <uovo.aureo.000@gmail.com>
2016-12-31 14:10:51 +0900

    add nodebrew stable

--- setup_centos.sh
+++ setup_centos.sh
@@ -2,3 +2,6 @@
 curl -L git.io/nodebrew | perl - setup
 echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.bash_profile
 source ~/.bash_profile
+exec $SHELL -l
+nodebrew install-binary stable
+nodebrew use stable
