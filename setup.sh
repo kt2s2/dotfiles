ac8ba23cfc7f6e5c7bf5d1f9555671a78cb72eaa
sa-inu <uovo.aureo.000@gmail.com>
2017-04-23 13:29:53 +0900

    [hotupdate] setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,55 @@
 #!/bin/bash
 
-DOT_FILES=(.bashrc .bash_profile .zshrc .vimrc .gvimrc .gitconfig .gitignore .vim .tmux.conf getwifi.sh get_volume_tmux.sh .pryrc default_gems .eslintrc)
+echo Start setup
 
-for file in ${DOT_FILES[@]}
-do
-  ln -s $HOME/src/github.com/sa-inu/dotfiles/linked/$file $HOME/$file
+echo What OS do you use?
+ans1="OSX"
+ans2="Ubuntu"
+ans3="CentOS"
+select ANS in "$ans1" "$ans2" "$ans3"; do
+  case "$ANS" in
+    "$ans1")
+      brew install tmux
+      break
+      ;;
+    "$ans2")
+      sudo apt-get -y update
+      sudo apt-get -y install build-essential
+      break
+      ;;
+    "$ans3")
+      sudo yum -y update
+      break
+      ;;
+    *)
+      echo 無効です
+      ;;
+  esac
 done
 
-source $HOME/.bash_profile
-source $HOME/.bashrc
-source $HOME/.zshrc
-exec $SHELL
+echo Symbolic link settings...
+files=`find . -maxdepth 1 -type f | sed "s!^.*/!!" | grep -vE ".DS_Store"`
+for file in ${files[@]}
+do
+  ln -s `pwd`/linked/$file $HOME/$file
+done
+
+echo Vim settings...
+if [ -e $HOME/.vim/backup ]; then
+  echo Already exist $HOME/.vim/backup
+else
+  mkdir -p $HOME/.vim/backup
+  echo Created $HOME/.vim/backup
+fi
+
+echo Neobundle settings...
+if [ -e $HOME/.vim/bundle ]; then
+  echo Already installed neobundle
+else
+  mkdir -p $HOME/.vim/bundle
+  echo Created $HOME/.vim/bundle
+  git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
+  echo Installed neobundle
+fi
+
+echo Load shell file
