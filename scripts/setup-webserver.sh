f2b8d9b7fa8421e211e8519160028283ddf26611
sa-inu <uovo.aureo.000@gmail.com>
2017-07-23 21:39:53 +0900

    fix

--- scripts/setup-webserver.sh
+++ scripts/setup-webserver.sh
@@ -5,13 +5,13 @@ ans2="apache"
 select ANS in $ans1 $ans2; do
   case $ANS in
     $ans1)
+      break;;
+    $ans2)
       sudo yum -y install httpd
       sudo service httpd start
       sudo chkconfig httpd on
       sudo service iptables stop
       sudo chkconfig iptables off
-      break;;
-    $ans2)
       break;;
   esac
 done
