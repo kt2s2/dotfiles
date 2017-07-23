200e36b85c3c49234ac2fddaf49f520bc9d768f2
sa-inu <uovo.aureo.000@gmail.com>
2017-07-23 22:28:45 +0900

    fix

--- scripts/centos/php-install.sh
+++ scripts/centos/php-install.sh
@@ -10,7 +10,9 @@ select ANS in $ans1 $ans2 $ans3; do
       echo "SKIP"
       break;;
     $ans1)
-      sudo yum -y install --enablerepo=remi,remi-php56 php php-devel php-mbstring php-pdo php-gd php-xml php-mcrypt;;
+      sudo rpm -Uvh http://ftp.iij.ad.jp/pub/linux/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
+      sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+      sudo yum -y install --enablerepo=remi --enablerepo=remi-php56 php php-opcache php-devel php-mbstring php-mcrypt php-mysqlnd php-phpunit-PHPUnit php-pecl-xdebug php-pecl-xhprof;;
     $ans2)
       sudo yum -y install --enablerepo=remi,remi-php70 php php-devel php-mbstring php-pdo php-gd php-xml php-mcrypt;;
     $ans3)
