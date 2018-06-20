0d06e723de92cfeb32502d1ff7d4bcbdcd57ec67
sainu <uovo.aureo.000@gmail.com>
2018-06-20 22:02:39 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,6 +24,11 @@ fi
 # Install homebrew
 which brew > /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install git
+
+mkdir -p ~/src/github.com/sa-inu/dotfiles
+git clone git@github.com:sa-inu/dotfiles.git ~/src/github.com/sa-inu/dotfiles
+
 # Install serverkit and its dependencies
 sudo gem install bundler
 sudo bundle install
