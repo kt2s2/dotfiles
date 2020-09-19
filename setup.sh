6f71c48465d249b8fa4c78862dec3f867c4da36a
sainu <saino.katsutoshi@moneyforward.co.jp>
2020-09-19 18:22:02 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,6 @@ mkdir -p ~/.vim/rc
 echo Symbolic link settings...
 files=`find home -type f | sed -e 's/home\///g'`
 for file in ${files[@]}; do
+  unlink $HOME/$file
   ln -s `pwd`/home/$file $HOME/$file
 done
