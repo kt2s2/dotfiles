d69b117ccb1560acbaf91693b825bb4595176db2
Katsutoshi Saino <saino.katsutoshi@moneyforward.co.jp>
2022-08-11 03:26:51 +0900

    Update install-macos-pkgs.sh

--- install-macos-pkgs.sh
+++ install-macos-pkgs.sh
@@ -38,3 +38,6 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 echo 'source "${ZDOTDIR:-$HOME}/.myzshrc.zsh"' >> "${ZDOTDIR:-$HOME}/.zshrc"
+
+# === mysql ===
+brew install mysql@5.7
